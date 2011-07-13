3ec6eb940bcf6e71c1b821df7c7209833e191530
Chad Dollins <chad.dollins@gmail.com>
2011-07-13 00:45:00 -0500

    Encoder now uses BufferOutputStream as suggested by Alexey. Also rev'd the version in pom

--- src/main/java/org/glassfish/grizzly/utils/ProtobufEncoder.java
+++ src/main/java/org/glassfish/grizzly/utils/ProtobufEncoder.java
@@ -1,10 +1,13 @@
 package org.glassfish.grizzly.utils;
 
 import org.glassfish.grizzly.*;
+import org.glassfish.grizzly.memory.MemoryManager;
 import org.glassfish.grizzly.attributes.AttributeStorage;
 import com.google.protobuf.Message;
 
 import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.io.IOException;
 
 /**
  * Protocol Buffer Encoder
@@ -12,6 +15,7 @@ import java.util.logging.Logger;
  * @author Chad Dollins
  */
 public class ProtobufEncoder extends AbstractTransformer<Message,Buffer> {
+    public static final int IO_ERROR_FROM_BUFFER_OUTPUT_STREAM = 0;
     private static final Logger logger = Grizzly.logger(ProtobufEncoder.class);
     private static final int LENGTH_HEADER_SIZE = 4;
 
@@ -21,18 +25,23 @@ public class ProtobufEncoder extends AbstractTransformer<Message,Buffer> {
             throw new TransformationException("Input could not be null");
         }
 
-        final byte[] byteRepresentation = input.toByteArray();
+        final MemoryManager mm = obtainMemoryManager(storage);
+        final BufferOutputStream bos = new BufferOutputStream(mm);
 
-        final Buffer output =
-                obtainMemoryManager(storage).allocate(byteRepresentation.length + LENGTH_HEADER_SIZE);
+        try {
+            bos.write(new byte[LENGTH_HEADER_SIZE]);
+            input.writeTo(bos);
+            bos.close();
+        } catch (IOException e) {
+            final String msg = "IOException during construction of BufferOutputStream from Protocol Buffer.";
+            logger.log(Level.WARNING, msg);
+            return TransformationResult.createErrorResult(IO_ERROR_FROM_BUFFER_OUTPUT_STREAM, msg);
+        }
 
-        output.putInt(byteRepresentation.length);
 
-        output.put(byteRepresentation);
-
-        output.flip();
-        output.allowBufferDispose(true);
-
+        final Buffer output = bos.getBuffer().flip();
+        final int len = output.remaining() - LENGTH_HEADER_SIZE;
+        output.putInt(0, len);
         return TransformationResult.createCompletedResult(output, null);
     }
 
